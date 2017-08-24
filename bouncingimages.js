330ba20315128961636e8bd65cca0a1fa1464af5
aoife2000 <aoifelouisemcmanus@gmail.com>
2017-08-24 13:40:41 -0700

    Update bouncingimages.js

--- bouncingimages.js
+++ bouncingimages.js
@@ -18,7 +18,7 @@ bouncingImages = new Object();
 bouncingImages.imgCount = 3;
 bouncingImages.imgWidth = 40;
 bouncingImages.imgHeight = 40;
-bouncingImages.imgSrc = 'superball.gif';
+bouncingImages.imgSrc = 'beer.jpg';
 
 bouncingImages.frameRate = 30;
 
